770bc1bd653409ab3b94b12c91ed731f32ab6d44
tobbie <zidan.guo186@gmail.com>
2023-10-15 15:14:33 +1100

    re-create react app on documents-meta-form-frontend

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -8,15 +7,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit
-          <code>
-            src/App.js
-          </code>
-          and save to reload.
+          Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Learn React
-        </p>
+        </a>
       </header>
     </div>
   );
