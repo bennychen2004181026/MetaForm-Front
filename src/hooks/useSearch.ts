acc9f4d671acb4a7e5242f789a1de4b73b8b49cc
tobbie <zidan.guo186@gmail.com>
2023-10-24 14:40:27 +1100

    prettier check

--- src/hooks/useSearch.ts
+++ src/hooks/useSearch.ts
@@ -1,9 +1,9 @@
-import {useState} from "react";
+import { useState } from 'react';
 
-export default function useSearch (){
-    const [search,setSearch] = useState<string>()
-    return{
+export default function useSearch() {
+    const [search, setSearch] = useState<string>();
+    return {
         search,
-        setSearch
-    }
+        setSearch,
+    };
 }
