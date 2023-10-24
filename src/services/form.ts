43dbdc7b0d84759e93187accad1ec530825f8647
tobbie <zidan.guo186@gmail.com>
2023-10-24 15:06:40 +1100

    change services=>user.tsx to user.ts same as form.ts due to might not use React in Services

---
+++ src/services/form.ts
@@ -0,0 +1,11 @@
+// import axios from 'axios';
+
+// // submit form
+// export const submitFormData = async (formData: FormData) => {
+//   try {
+//     const response = await axios.post('/api/submit', formData);
+//     return response.data;
+//   } catch (error) {
+//     throw error;
+//   }
+// }
