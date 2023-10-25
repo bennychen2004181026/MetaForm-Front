6a8551b1035268b8e870197e9b84cf6bb29df2a6
tobbie <zidan.guo186@gmail.com>
2023-10-25 14:14:12 +1100

    change class style to styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
 import React from 'react';
 
+import styled from 'styled-components';
+
 import AppRoute from '@/routes/AppRoute';
 
 const App = () => {
-    const initDivTestStyle = {
-        backgroundColor: 'lightblue',
-        padding: '20px',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '100vh',
-    };
+    const CUSTOM_BLUE = '#4285f4';
+    const Container = styled.div`
+        align-items: center;
+        background: ${CUSTOM_BLUE};
+        display: flex;
+        height: 100vh;
+        justify-content: center;
+        padding: 20px;
+    `;
     return (
-        <div className="initDivTest" style={initDivTestStyle}>
+        <Container>
             <AppRoute />
-        </div>
+        </Container>
     );
 };
 
