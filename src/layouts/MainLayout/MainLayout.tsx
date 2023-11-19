513c4c108a8dcb8269157858ecd8b712a8ea5cfb
Ben <ben83026@outlook.com>
2023-11-19 18:44:25 +1100

    Create a MainLayout component

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
 
-const MainLayout = () => {
-    <div>MainLayout</div>;
+import styled from 'styled-components';
+
+const Container = styled.div`
+    padding: 20px;
+    margin: 0 auto;
+    max-width: 1200px;
+    min-height: 100%;
+`;
+
+interface MainLayoutProps {
+    children: React.ReactNode;
+    className?: string;
+}
+
+const MainLayout: React.FC<MainLayoutProps> = ({ children, className }) => {
+    return <Container className={className}>{children}</Container>;
 };
 
 export default MainLayout;
