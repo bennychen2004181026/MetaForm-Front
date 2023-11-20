95c4d39faebf978f9a6d99ceda446179ffcf20ab
Dylan-li <liyansong2abc@gmail.com>
2023-11-21 00:30:59 +1100

    update mainPage route

--- src/routes/AppRoute.tsx
+++ src/routes/AppRoute.tsx
@@ -3,17 +3,15 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import ForgetPasswordPage from '@/pages/ForgetPasswordPage';
-import HomePage from '@/pages/HomePage';
 import MainPage from '@/pages/MainPage';
 import RegisterPage from '@/pages/RegisterPage';
 
 const AppRoute = () => (
     <BrowserRouter>
         <Routes>
-            <Route path="/main" element={<MainPage />} />
+            <Route path="/" element={<MainPage />} />
             <Route path="/register" element={<RegisterPage />} />
             <Route path="/forgetpassword" element={<ForgetPasswordPage />} />
-            <Route path="/home" element={<HomePage />} />
         </Routes>
     </BrowserRouter>
 );
