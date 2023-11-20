57b1e2c26812fced37d05d2c18e1f8abe9f93bca
Ben <ben83026@outlook.com>
2023-11-20 16:53:07 +1100

    Create simple StyledButton

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import styled from 'styled-components';
 
-const Button = () => {
-    <div>
-        <button type="button">Button</button>
-    </div>;
-};
+const StyledButton = styled.button`
+    background-color: #007bff;
+    color: white;
+    padding: 10px 15px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    font-size: 16px;
+    transition: background-color 0.2s;
 
-export default Button;
+    &:hover {
+        background-color: #0056b3;
+    }
+`;
+
+export default StyledButton;
