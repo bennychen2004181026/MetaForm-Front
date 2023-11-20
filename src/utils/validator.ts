31adf4553ec8a1385ad7c12c57a5122aa88e5626
Ben <ben83026@outlook.com>
2023-11-20 12:55:39 +1100

    Install lodash and create validators

--- src/utils/validator.ts
+++ src/utils/validator.ts
@@ -1,8 +1,15 @@
-export {};
+import { isEmpty } from 'lodash';
 
-// const validateEmail = (email: string): boolean => {
-//     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-//     return emailRegex.test(email);
-// };
+const PASSWORD_REGEX = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^_&*]).{8,32}$/;
 
-// export default validateEmail;
+const validators = {
+    isReuqired: (label: string, value: string) => (!isEmpty(value) ? '' : `Please enter ${label}!`),
+    validatePassword: (password: string) =>
+        PASSWORD_REGEX.test(password) && !isEmpty(password)
+            ? ''
+            : 'Password should contain at least one number, one lowercase letter, one uppercase letter and one special character (@,#,$,%,^,_,&,*,!). And 8 to 32 characters long',
+    validateConfirmPassword: (password: string, confirmPassword: string) =>
+        password === confirmPassword ? '' : 'Password and Confirm Password does not match.',
+};
+
+export default validators;
