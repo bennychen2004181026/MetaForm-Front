62c9a2e6421522c4369a16507582cbe53c7a8400
Oliver <oliverwang008@gmail.com>
2023-11-24 22:54:12 +1100

    update button code based on feedback

--- src/pages/RegisterPage/RegisterPage.tsx
+++ src/pages/RegisterPage/RegisterPage.tsx
@@ -1,35 +1,68 @@
 import React from 'react';
 
-import { Typography } from '@mui/material';
+import { Button, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
-import EmailButton from './components/EmailButton';
-import GoogleButton from './components/GoogleButton';
+import googleIcon from '@/assets/images/google-icon-logo.png';
+import Title from '@/layouts/MainLayout/Title';
+import GlobalStyle from '@/styles/GlobalStyle';
 
 const Content = styled.div`
-    background-color: white;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
 `;
 
-const Title = styled.strong`
-    font-size: 38px;
+const RegisterButton = styled(Button)`
+    width: 320px;
+    height: 52px;
+    margin: 0px;
+    border: 1px;
+    border-style: solid;
+    border-radius: 15px;
+    border-color: black;
+    padding: 8px 50px;
     font-weight: 600;
-    font-family: Arial, Helvetica, sans-serif;
-    margin-bottom: 35px;
+    font-size: 15px;
+    text-transform: none;
+    text-decoration: none;
+`;
+
+const GoogleIcon = styled.img`
+    margin-right: 10px;
+    width: 22px;
+    height: 22px;
 `;
 
 const RegisterPage = () => {
     return (
         <Content>
-            <Title>Create your account</Title>
-            <GoogleButton />
+            <GlobalStyle />
+            <Title content="Create your account" />
+            <Link to="signupGoogle">
+                <RegisterButton
+                    startIcon={<GoogleIcon src={googleIcon} />}
+                    sx={{ color: 'black', backgroundColor: 'white' }}
+                >
+                    Sign up with Google
+                </RegisterButton>
+            </Link>
             <Typography variant="subtitle1" padding="15px">
                 or
             </Typography>
-            <EmailButton />
+            <Link to="signupEmail">
+                <RegisterButton
+                    sx={{
+                        color: 'white',
+                        backgroundColor: 'black',
+                        '&:hover': { bgcolor: 'grey' },
+                    }}
+                >
+                    Sign up with email
+                </RegisterButton>
+            </Link>
         </Content>
     );
 };
