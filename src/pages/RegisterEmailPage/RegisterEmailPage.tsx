e0f23ebdc87dbe43319f9512ac71149f3303bc4d
Oliver <oliverwang008@gmail.com>
2023-11-30 01:26:59 +1100

    add register email page

--- src/pages/RegisterEmailPage/RegisterEmailPage.tsx
+++ src/pages/RegisterEmailPage/RegisterEmailPage.tsx
@@ -1,66 +1,98 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import {
-    Button,
-    Checkbox,
-    FormControlLabel,
-    FormGroup,
-    TextField,
-    Typography,
-} from '@mui/material';
-import { Link } from 'react-router-dom';
+import { Checkbox, FormControlLabel, FormGroup, TextField, Typography } from '@mui/material';
 import styled from 'styled-components';
 
+import StyledButton from '@/components/Button/Button';
+import useForm from '@/hooks/useForm';
 import Title from '@/layouts/MainLayout/Title';
 import GlobalStyle from '@/styles/GlobalStyle';
+import validator from '@/utils/UserRegisterFormValidators';
 
 const Content = styled.div`
     display: flex;
     flex-direction: column;
-    align-items: flex-start;
+    align-items: center;
     justify-conetnet: center;
     width: 30%;
 `;
 
-const InputField = styled(TextField)`
-    width: 350px;
-    margin-bottom: 30px;
+interface CustomFieldProps {
+    customMargin?: string;
+}
+const StyledTextField = styled(TextField)<CustomFieldProps>`
+    margin-bottom: ${(props) => props.customMargin};
 `;
+const registerEmailType = (formField: Record<string, string>) => [
+    {
+        id: 1,
+        label: 'User Name',
+        getErrorMessage: (data: Record<string, string>) =>
+            validator.isRequired('UserName', data?.username),
+        key: 'username',
+        value: formField.username,
+        margin: '1rem',
+    },
+    {
+        id: 2,
+        label: 'Work Email',
+        getErrorMessage: (data: Record<string, string>) => validator.validEmail(data?.workEmail),
+        key: 'workEmail',
+        value: formField.workEmail,
+        margin: '1rem',
+    },
+];
 
 const RegisterEmail = () => {
+    const [formData, setFormData] = useState({
+        username: '',
+        workEmail: '',
+    });
+    const formField = registerEmailType(formData);
+    const { data, focus, onBlur, onChange, validation } = useForm(formField);
     return (
         <Content>
+            <GlobalStyle />
+            <Title content="Create your account" />
+            {formField.map((type) => {
+                return (
+                    <StyledTextField
+                        key={type.id}
+                        label={type.label}
+                        variant="outlined"
+                        fullWidth
+                        helperText={(focus[type.key] && type.getErrorMessage?.(data)) ?? ' '}
+                        onBlur={onBlur(type.key)}
+                        onChange={onChange(type.key)}
+                        error={focus[type.key] && !!type.getErrorMessage?.(data)}
+                        customMargin={type.margin}
+                    />
+                );
+            })}
+            <Typography variant="subtitle1" sx={{ padding: '5px', alignSelf: 'flex-start' }}>
+                Opt-in
+            </Typography>
             <FormGroup>
-                <GlobalStyle />
-                <Title content="Create your account" />
-                <InputField id="username" label="Username" variant="outlined" />
-                <InputField id="workemail" label="Work Email" variant="outlined" />
-                <Typography variant="subtitle1" padding="5px">
-                    Opt-in
-                </Typography>
                 <FormControlLabel
                     control={<Checkbox />}
-                    id="marketingemails"
+                    id="marketingEmail"
                     label="Marketing Emails"
                 />
                 <FormControlLabel
                     control={<Checkbox />}
-                    id="newsemails"
+                    id="newsEmail"
                     label="News & Updates Emails"
                 />
                 <FormControlLabel
+                    id="termsAgreement"
                     required
                     control={<Checkbox />}
-                    id="termsconditions"
                     label="I agree to MetaForm's Terms of Service, Privacy Policy and Data Processing Agreement"
                 />
-                <Button
-                    variant="contained"
-                    sx={{ width: '300px', marginTop: '35px', alignSelf: 'center' }}
-                >
-                    Create My Account
-                </Button>
             </FormGroup>
+            <StyledButton variant="contained" sx={{ margin: '2rem' }}>
+                Create my account
+            </StyledButton>
         </Content>
     );
 };
