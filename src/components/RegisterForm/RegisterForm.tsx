f99a24567646944b4e5dc9a49c81c86d0c3652f6
Ben <ben83026@outlook.com>
2023-11-29 16:11:28 +1100

    Refactor RegisterForm

--- src/components/RegisterForm/RegisterForm.tsx
+++ src/components/RegisterForm/RegisterForm.tsx
@@ -1,81 +1,119 @@
 import React from 'react';
 
-import { useSnackbar } from 'notistack';
+import { Box } from '@mui/system';
+import { useLocation } from 'react-router-dom';
 
 import StyledButton from '@/components/Button';
 import ReusableForm from '@/components/ReusableForm';
 import { IField, useForm } from '@/hooks/useForm1';
-
-const formFields: IField[] = [
-    {
-        id: 1,
-        label: 'First Name',
-        key: 'firstName',
-        type: 'text',
-        value: '',
-        validationRules: [{ key: 'isRequired', additionalData: 'First Name' }],
-    },
-    {
-        id: 2,
-        label: 'Last Name',
-        key: 'lastName',
-        type: 'text',
-        value: '',
-        validationRules: [{ key: 'isRequired', additionalData: 'Last Name' }],
-    },
-    {
-        id: 3,
-        label: 'Password',
-        key: 'password',
-        type: 'text',
-        value: '',
-        validationRules: [
-            { key: 'isRequired', additionalData: 'Password' },
-            { key: 'validatePassword' },
-        ],
-    },
-    {
-        id: 4,
-        label: 'Confirm Password',
-        key: 'confirmPassword',
-        type: 'text',
-        value: '',
-        validationRules: [
-            { key: 'isRequired', additionalData: 'Confirm Password' },
-            { key: 'validateConfirmPassword', additionalData: 'password' },
-        ],
-    },
-];
+import useSnackbarHelper from '@/utils/useSnackbarHelper';
 
 const RegisterForm = () => {
-    const { enqueueSnackbar } = useSnackbar();
-    const { data, focus, errors, onChange, onBlur, resetForm, isValid } = useForm(formFields);
+    const location = useLocation();
+    const showSnackbar = useSnackbarHelper();
+    // Former page delivers with email, username in the location.state
+    const { email, username } = location.state || {};
+    const formFields: IField[] = [
+        {
+            id: 1,
+            label: 'First Name',
+            key: 'firstName',
+            type: 'input',
+            value: '',
+            validationRules: [{ key: 'isRequired', additionalData: 'First Name' }],
+        },
+        {
+            id: 2,
+            label: 'Last Name',
+            key: 'lastName',
+            type: 'input',
+            value: '',
+            validationRules: [{ key: 'isRequired', additionalData: 'Last Name' }],
+        },
+        {
+            id: 3,
+            label: 'Password',
+            key: 'password',
+            type: 'input',
+            value: '',
+            validationRules: [
+                { key: 'isRequired', additionalData: 'Password' },
+                { key: 'validatePassword' },
+            ],
+        },
+        {
+            id: 4,
+            label: 'Confirm Password',
+            key: 'confirmPassword',
+            type: 'input',
+            value: '',
+            validationRules: [
+                { key: 'isRequired', additionalData: 'Confirm Password' },
+                { key: 'validateConfirmPassword', additionalData: 'password' },
+            ],
+        },
+        {
+            id: 5,
+            label: 'Email',
+            key: 'email',
+            type: 'input',
+            value: email,
+            validationRules: [],
+        },
+        {
+            id: 6,
+            label: 'User name',
+            key: 'username',
+            type: 'input',
+            value: username,
+            validationRules: [],
+        },
+    ];
 
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    const { data, focus, errors, onChange, onBlur, resetForm, isValid, validateAllFields } =
+        useForm(formFields);
+
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        if (isValid()) {
-            // Handle form submission logic here
-            enqueueSnackbar('handleSubmit logic triggered', {
-                variant: 'info',
-            });
+        if (!validateAllFields()) {
+            showSnackbar('Please fill all the required valid fields first', 'error');
+        } else {
+            // Logic with handleSubmit
+            showSnackbar('You had successfully created account', 'success');
         }
     };
 
     return (
-        <ReusableForm
-            formFields={formFields}
-            data={data}
-            focus={focus}
-            errors={errors}
-            onChange={onChange}
-            onBlur={onBlur}
-            isValid={isValid}
-            handleSubmit={handleSubmit}
+        <Box
+            sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+                py: 8,
+            }}
         >
-            <StyledButton type="button" onClick={resetForm} variant="contained" color="secondary">
-                Reset
-            </StyledButton>
-        </ReusableForm>
+            <ReusableForm
+                excludeFields={['email', 'username']}
+                formFields={formFields}
+                data={data}
+                focus={focus}
+                errors={errors}
+                onChange={onChange}
+                onBlur={onBlur}
+                isValid={isValid}
+                handleSubmit={handleSubmit}
+            >
+                <StyledButton
+                    type="button"
+                    onClick={resetForm}
+                    variant="contained"
+                    color="secondary"
+                >
+                    Reset
+                </StyledButton>
+            </ReusableForm>
+        </Box>
     );
 };
 
