591295a3ec20f8503238f99d70ba42e3c3bbc8ea
Ben <ben83026@outlook.com>
2023-11-29 16:46:52 +1100

    Add routes for two pages

--- src/routes/AppRoute.tsx
+++ src/routes/AppRoute.tsx
@@ -5,13 +5,16 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import CompanyRegisterPage from '@/pages/CompanyRegisterPage';
 import ForgetPasswordPage from '@/pages/ForgetPasswordPage';
 import MainPage from '@/pages/MainPage';
-import RegisterPage from '@/pages/RegisterPage';
+
+const EmailLinkVerificationPage = React.lazy(() => import('@/pages/EmailLinkVerificationPage'));
+const RegisterPage = React.lazy(() => import('@/pages/RegisterPage'));
 
 const AppRoute = () => (
     <BrowserRouter>
         <Routes>
             <Route path="/" element={<MainPage />} />
-            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/verification/:token" element={<EmailLinkVerificationPage />} />
+            <Route path="/create-user" element={<RegisterPage />} />
             <Route path="/forgetpassword" element={<ForgetPasswordPage />} />
             <Route path="/companyRegister" element={<CompanyRegisterPage />} />
         </Routes>
