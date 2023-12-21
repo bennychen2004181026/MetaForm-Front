460cf16c5476dbb422ece432c86af03da160c969
Ben <ben83026@outlook.com>
2023-12-21 14:19:16 +1100

    Create createEmployee stepperOne content

---
+++ src/pages/CreateEmployeePage/components/StepContentOne/StepContentOne.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+
+import styled from 'styled-components';
+
+import ReusableForm from '@/components/ReusableForm';
+import { IField } from '@/hooks/useForm';
+
+const StyledStepperBoxContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    @media (min-width: 768px) {
+        flex-direction: row;
+    }
+    width: 360px;
+    height: 260px;
+    @media (min-width: 600px) {
+        width: 500px;
+        height: 400px;
+    }
+    @media (min-width: 960px) {
+        width: 600px;
+        height: 500px;
+    }
+    justify-content: space-between;
+    align-items: center;
+    margin-top: 1px;
+    gap: 1rem;
+`;
+interface StepContentOneProps {
+    formFields: IField[];
+    fieldsData: Record<string, string>;
+    onDataChange: (
+        field: string,
+    ) => (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement> | string) => void;
+    fieldsFocus: Record<string, boolean>;
+    errors: Record<string, string>;
+    onFieldsBlur: (field: string) => () => void;
+    isValid: () => boolean;
+    showSubmitButton: boolean;
+}
+
+const StepContentOne: React.FC<StepContentOneProps> = ({
+    formFields,
+    fieldsData,
+    onDataChange,
+    fieldsFocus,
+    errors,
+    onFieldsBlur,
+    isValid,
+    showSubmitButton,
+}) => {
+    return (
+        <StyledStepperBoxContainer>
+            <ReusableForm
+                excludeFields={['password', 'confirmPassword']}
+                formFields={formFields}
+                fieldsData={fieldsData}
+                onDataChange={onDataChange}
+                fieldsFocus={fieldsFocus}
+                errors={errors}
+                onFieldsBlur={onFieldsBlur}
+                isValid={isValid}
+                showSubmitButton={showSubmitButton}
+            />
+        </StyledStepperBoxContainer>
+    );
+};
+
+export default StepContentOne;
