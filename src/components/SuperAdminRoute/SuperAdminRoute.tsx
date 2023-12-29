4935c641bd1f48c12ad93ea19b55a1656c91837c
Ben <ben83026@outlook.com>
2023-12-29 17:00:43 +1100

    Create SuperAdminRoute

---
+++ src/components/SuperAdminRoute/SuperAdminRoute.tsx
@@ -0,0 +1,23 @@
+import React, { useEffect } from 'react';
+
+import { Outlet } from 'react-router-dom';
+
+import { useAppSelector } from '@/hooks/redux';
+import { Role } from '@/interfaces/User.interface';
+import { authUserRole } from '@/store/slices/auth/authSlice';
+import useSnackbarHelper from '@/utils/useSnackbarHelper';
+
+const SuperAdminRoute = () => {
+    const showSnackbar = useSnackbarHelper();
+    const fetchUserRole: Role = useAppSelector(authUserRole);
+
+    useEffect(() => {
+        if (fetchUserRole === Role.Admin) {
+            showSnackbar('Unauthorized Super Admin access', 'warning');
+        }
+    }, [fetchUserRole, showSnackbar]);
+
+    return <Outlet />;
+};
+
+export default SuperAdminRoute;
