2536801640fcccb9a933107f0290a78b20153cc3
Yansong Li <liyansong2abc@gmail.com>
2023-12-30 05:49:19 +1100

    ss

--- src/pages/CreateFormPage/components/NewQuestion/components/FileUploadQuestion/FileUploadQuestion.tsx
+++ src/pages/CreateFormPage/components/NewQuestion/components/FileUploadQuestion/FileUploadQuestion.tsx
@@ -1,8 +1,7 @@
 import * as React from 'react';
 
-import { FormControl, InputLabel, ListItemText, MenuItem, OutlinedInput } from '@mui/material';
+import { FormControl, ListItemText, MenuItem, OutlinedInput } from '@mui/material';
 import Checkbox from '@mui/material/Checkbox';
-import FormControlLabel from '@mui/material/FormControlLabel';
 import FormGroup from '@mui/material/FormGroup';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 
