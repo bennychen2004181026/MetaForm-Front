cde070c4dfc64732c8fe76d0e44e6b928d827746
Yansong Li <liyansong2abc@gmail.com>
2024-01-02 23:43:56 +1100

    form card

--- src/pages/FormListPage/components/FormsCards/components/FormCards.tsx
+++ src/pages/FormListPage/components/FormsCards/components/FormCards.tsx
@@ -1,33 +1,48 @@
 import React from 'react';
 
+import { Paper } from '@mui/material';
 import Grid from '@mui/material/Grid';
-// import { makeStyles } from '@mui/styles';
+import styled from 'styled-components';
 
 import FormCard from './FormCard/FormCard';
 
-// const useStyles = makeStyles((theme) => ({
-//     gridContainer: {
-//         padding: '0 20px',
-//     },
-// }));
+const StyledGridContainer = styled(Grid)`
+    padding: 0 20px;
+`;
 
 const FormCards = () => {
-    // const classes = useStyles();
     return (
-        <Grid container spacing={4}>
-            <Grid item xs={12} sm={6} md={4}>
-                <FormCard />
-            </Grid>
-            <Grid item xs={12} sm={6} md={4}>
-                <FormCard />
-            </Grid>
-            <Grid item xs={12} sm={6} md={4}>
-                <FormCard />
-            </Grid>
-            <Grid item xs={12} sm={6} md={4}>
-                <FormCard />
-            </Grid>
-        </Grid>
+        <Paper>
+            <StyledGridContainer container spacing={4}>
+                <Grid item xs={12} sm={6} md={4} lg={3}>
+                    <FormCard
+                        formTitle="Test Form by Monash university sadasdasdasdasda"
+                        numOfResponses={10}
+                    />
+                </Grid>
+                <Grid item xs={12} sm={6} md={4} lg={3}>
+                    <FormCard
+                        formTitle="Test Form by Monash university sadasdasdasdasda"
+                        numOfResponses={10}
+                    />
+                </Grid>
+                <Grid item xs={12} sm={6} md={4} lg={3}>
+                    <FormCard formTitle="Test Form by Monash university" numOfResponses={10} />
+                </Grid>
+                <Grid item xs={12} sm={6} md={4} lg={3}>
+                    <FormCard formTitle="Test Form by Monash university" numOfResponses={10} />
+                </Grid>
+                <Grid item xs={12} sm={6} md={4} lg={12}>
+                    <FormCard formTitle="Test Form by Monash university" numOfResponses={10} />
+                </Grid>
+                <Grid item xs={12} sm={6} md={4} lg={12}>
+                    <FormCard formTitle="Test Form by Monash university" numOfResponses={10} />
+                </Grid>
+                <Grid item xs={12} sm={6} md={4} lg={3}>
+                    <FormCard formTitle="Test Form by Monash university" numOfResponses={10} />
+                </Grid>
+            </StyledGridContainer>
+        </Paper>
     );
 };
 
