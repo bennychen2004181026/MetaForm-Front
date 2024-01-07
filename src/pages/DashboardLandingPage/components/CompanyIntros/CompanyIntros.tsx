19ae28c05866165738c1c7a3e6b5eb794ec71dcd
Ben <ben83026@outlook.com>
2024-01-07 11:21:10 +1100

    Attach useGetEmployeesQuery

--- src/pages/DashboardLandingPage/components/CompanyIntros/CompanyIntros.tsx
+++ src/pages/DashboardLandingPage/components/CompanyIntros/CompanyIntros.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { Badge, Business, BusinessCenter } from '@mui/icons-material/';
 import { Avatar, List, ListItem, ListItemAvatar, ListItemText } from '@mui/material/';
+import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
 
 import DefaultCompanyLogo from '@/assets/images/DefaultCompanyLogo.jpg';
+import { useAppDispatch, useAppSelector } from '@/hooks/redux';
+import { ApiError } from '@/interfaces/ApiError';
+import { IEmployeeInfo } from '@/interfaces/ICompany';
+import LoadingSpinner from '@/layouts/LoadingSpinner';
+import companyApis from '@/services/company';
+import {
+    myCompanyABN,
+    myCompanyIndustry,
+    myCompanyLogo,
+    myCompanyMembersInfo,
+    myCompanyName,
+    setEmployeesInfos,
+} from '@/store/slices/company/companySlice';
+import useSnackbarHelper from '@/utils/useSnackbarHelper';
 
 const StyledListItem = styled(ListItem)`
     display: flex;
@@ -29,11 +44,42 @@ const StyledListItemText = styled(ListItemText)`
 `;
 
 const CompanyIntros: React.FC = () => {
+    const { companyId } = useParams<{ companyId: string }>();
+    const showSnackbar = useSnackbarHelper();
+    const dispatch = useAppDispatch();
+    const companyName: string = useAppSelector(myCompanyName);
+    const companyABN: string = useAppSelector(myCompanyABN);
+    const companyLogo: string = useAppSelector(myCompanyLogo);
+    const companyIndustry: string = useAppSelector(myCompanyIndustry);
+    const companyMembers: IEmployeeInfo = useAppSelector(myCompanyMembersInfo);
+
+    const { useGetEmployeesQuery } = companyApis;
+    const { data, error, isLoading } = useGetEmployeesQuery(companyId as string);
+
+    useEffect(() => {
+        if (error) {
+            const apiError = error as ApiError;
+            const errorMessage =
+                apiError.data?.errors?.[0].message || apiError.data || 'An unknown error occurred';
+
+            showSnackbar(`statusCode: ${apiError.status}\nmessage: ${errorMessage}`, 'error');
+        }
+    }, [error]);
+
+    if (data) {
+        const { employeesArray } = data;
+        dispatch(setEmployeesInfos(employeesArray));
+    }
+
+    if (isLoading) {
+        return <LoadingSpinner />;
+    }
+
     return (
         <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
             <StyledListItem>
                 <ListItemAvatar>
-                    <Logo src={DefaultCompanyLogo} alt="Default Company Logo" />
+                    <Logo src={companyLogo || DefaultCompanyLogo} alt="Default Company Logo" />
                 </ListItemAvatar>
             </StyledListItem>
             <StyledListItem>
@@ -42,7 +88,7 @@ const CompanyIntros: React.FC = () => {
                         <BusinessCenter />
                     </Avatar>
                 </ListItemAvatar>
-                <StyledListItemText primary="Company Name" secondary="123123" />
+                <StyledListItemText primary="Company Name" secondary={`${companyName || 'None'}`} />
             </StyledListItem>
             <StyledListItem>
                 <ListItemAvatar>
@@ -50,7 +96,7 @@ const CompanyIntros: React.FC = () => {
                         <Badge />
                     </Avatar>
                 </ListItemAvatar>
-                <StyledListItemText primary="ABN" secondary="1231231" />
+                <StyledListItemText primary="ABN" secondary={`${companyIndustry || 'None'}`} />
             </StyledListItem>
             <StyledListItem>
                 <ListItemAvatar>
@@ -58,7 +104,7 @@ const CompanyIntros: React.FC = () => {
                         <Business />
                     </Avatar>
                 </ListItemAvatar>
-                <StyledListItemText primary="Industry" secondary="1231231" />
+                <StyledListItemText primary="Industry" secondary={`${companyABN || 'None'}`} />
             </StyledListItem>
         </List>
     );
