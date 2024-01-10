51a1c867127d4b0506495f9225f1785916ec8c43
Oliver <oliverwang008@gmail.com>
2024-01-10 22:58:48 +1100

    address conflict

--- src/routes/AppRoute.tsx
+++ src/routes/AppRoute.tsx
@@ -3,7 +3,6 @@ import React, { Suspense } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import LoadingSpinner from '@/layouts/LoadingSpinner';
-import CompanyMembersPage from '@/pages/CompanyMembersPage';
 import CompanyRegisterPage from '@/pages/CompanyRegisterPage';
 import CreateFormPage from '@/pages/CreateFormPage/CreateFormPage';
 import FormListPage from '@/pages/FormListPage';
@@ -58,10 +57,6 @@ const AppRoute = () => (
                     <Route path="/user-dashboard" />
                     <Route element={<CompanyDashboardRoute />}>
                         <Route path="/companies/:companyId/employees" />
-                        <Route
-                            path="/companies/:companyId/company-members"
-                            element={<CompanyMembersPage />}
-                        />
                         <Route path="/companies/:companyId/invite-employees" />
                         <Route element={<SuperAdminRoute />}>
                             <Route path="/companies/:companyId/users/:userId/update-company-profile" />
