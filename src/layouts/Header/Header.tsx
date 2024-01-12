d6457c94a942b0c0f97524682350392abcf53e12
Ben <ben83026@outlook.com>
2024-01-12 19:14:16 +1100

    Create AppBar

--- src/layouts/Header/Header.tsx
+++ src/layouts/Header/Header.tsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+import HomeIcon from '@mui/icons-material/Home';
+import LogoutIcon from '@mui/icons-material/Logout';
+import MenuIcon from '@mui/icons-material/Menu';
+import {
+    AppBar,
+    Drawer,
+    IconButton,
+    List,
+    ListItem,
+    ListItemIcon,
+    ListItemText,
+    Toolbar,
+} from '@mui/material';
 import { Link, useNavigate } from 'react-router-dom';
 
-import { Image, Nav, SignUpLink, StyledLink } from './Header.styles';
+import { Image, SignUpLink, StyledLink } from './Header.styles';
 import headerLogo from '@/assets/images/logo-jr-academy.png';
 
 const Header = () => {
@@ -11,8 +24,53 @@ const Header = () => {
         navigate('/home');
     };
 
+    const [drawerOpen, setDrawerOpen] = useState(false);
+
+    const toggleDrawer = (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+        if (
+            event.type === 'keydown' &&
+            ((event as React.KeyboardEvent).key === 'Tab' ||
+                (event as React.KeyboardEvent).key === 'Shift')
+        ) {
+            return;
+        }
+        setDrawerOpen(open);
+    };
+
+    const list = () => (
+        <div role="presentation" onClick={toggleDrawer(false)} onKeyDown={toggleDrawer(false)}>
+            <List>
+                <ListItem button>
+                    <ListItemIcon>
+                        <HomeIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Home" />
+                </ListItem>
+                <ListItem button>
+                    <ListItemIcon>
+                        <LogoutIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Logout" />
+                </ListItem>
+            </List>
+        </div>
+    );
+
     return (
-        <Nav>
+        <AppBar position="static">
+            <Toolbar>
+                <IconButton
+                    edge="start"
+                    color="inherit"
+                    aria-label="menu"
+                    onClick={toggleDrawer(true)}
+                >
+                    <MenuIcon />
+                </IconButton>
+            </Toolbar>
+            <Drawer anchor="left" open={drawerOpen} onClose={toggleDrawer(false)}>
+                {list()}
+            </Drawer>
             <Link to="/home">
                 <Image src={headerLogo} alt="loading" onClick={logoRedirectHandler} />
             </Link>
@@ -20,7 +78,7 @@ const Header = () => {
                 Already have an account?
                 <StyledLink to="/login">Log in</StyledLink>
             </SignUpLink>
-        </Nav>
+        </AppBar>
     );
 };
 
